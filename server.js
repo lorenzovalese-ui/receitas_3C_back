44a23fe8086c869da6f24a6232539f4daecda1b9
LORENZO OLIVEIRA VALESE <lorenzo_valese@estudante.sesisenai.org.br>
2026-03-02 11:51:29 -0300

    PA

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
+import { Pool } from 'pg';
+
+const sql = new Pool({
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+port: 5432,
+database: "receitas"
+
+})
 
 const servidor = Fastify()
 
-servidor.get('/usuarios', () => {
-    return 'esta funcionando'
+servidor.get('/usuarios', async () => {
+    const resultado = await sql.query('select * from usuario')
+    return resultado.rows
 })
 
+servidor.post("/usuarios", async (request,reply) => {
+    const nome = request.body.nome;
+    const senha = request.body.senha;
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)',[nome, senha])
+    return 'usuario Cadastrado!'
+})
 servidor.listen({
     port: 3000
 })
